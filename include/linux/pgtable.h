0b9df9cc42f7620d44b4b973d4d19574ce5161c4
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-02-07 20:25:34 +0200

    mm: cache struct page for empty_zero_page and return it from ZERO_PAGE()
    For most architectures every invocation of ZERO_PAGE() does
    virt_to_page(empty_zero_page). But empty_zero_page is in BSS and it is
    enough to get its struct page once at initialization time and then use
    it whenever a zero page should be accessed.
    
    Add yet another __zero_page variable that will be initialized as
    virt_to_page(empty_zero_page) for most architectures in a weak
    arch_setup_zero_pages() function.
    
    For architectures that use colored zero pages (MIPS and s390) rename their
    setup_zero_pages() to arch_setup_zero_pages() and make it global rather
    than static.
    
    For architectures that cannot use virt_to_page() for BSS (arm64 and
    sparc64) add override of arch_setup_zero_pages().
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- include/linux/pgtable.h
+++ include/linux/pgtable.h
@@ -1925,6 +1925,8 @@ static inline void pfnmap_setup_cachemode_pfn(unsigned long pfn, pgprot_t *prot)
  * For architectures that don't __HAVE_COLOR_ZERO_PAGE the zero page lives in
  * empty_zero_page in BSS.
  */
+void arch_setup_zero_pages(void);
+
 extern unsigned long zero_page_pfn;
 
 #ifdef __HAVE_COLOR_ZERO_PAGE
@@ -1949,10 +1951,13 @@ static inline unsigned long zero_pfn(unsigned long addr)
 }
 
 extern uint8_t empty_zero_page[PAGE_SIZE];
+extern struct page *__zero_page;
 
-#ifndef ZERO_PAGE
-#define ZERO_PAGE(vaddr) ((void)(vaddr),virt_to_page(empty_zero_page))
-#endif
+static inline struct page *_zero_page(unsigned long addr)
+{
+	return __zero_page;
+}
+#define ZERO_PAGE(vaddr) _zero_page(vaddr)
 
 #endif /* __HAVE_COLOR_ZERO_PAGE */
 

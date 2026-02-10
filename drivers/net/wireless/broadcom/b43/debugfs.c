930b92911f091e5a8da55b10ff9e88abb84de12b
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-02-10 18:30:43 +0200

    b43: replace __get_free_pages() with kzalloc()
    The b43_debugfs_write() allocates memory with __get_free_pages() and uses
    it as a kernel buffer that does not require access to page metadata. Such
    usage can be served by the kzalloc() family.
    
    Replace use of __get_free_pages() with kzalloc().
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- drivers/net/wireless/broadcom/b43/debugfs.c
+++ drivers/net/wireless/broadcom/b43/debugfs.c
@@ -518,7 +518,7 @@ static ssize_t b43_debugfs_read(struct file *file, char __user *userbuf,
 	dfile = fops_to_dfs_file(dev, dfops);
 
 	if (!dfile->buffer) {
-		buf = (char *)__get_free_pages(GFP_KERNEL, buforder);
+		buf = kmalloc(PAGE_SIZE << (buforder), GFP_KERNEL);
 		if (!buf) {
 			err = -ENOMEM;
 			goto out_unlock;
@@ -526,7 +526,7 @@ static ssize_t b43_debugfs_read(struct file *file, char __user *userbuf,
 		memset(buf, 0, bufsize);
 		ret = dfops->read(dev, buf, bufsize);
 		if (ret <= 0) {
-			free_pages((unsigned long)buf, buforder);
+			kfree((void *)(unsigned long)buf);
 			err = ret;
 			goto out_unlock;
 		}
@@ -538,7 +538,7 @@ static ssize_t b43_debugfs_read(struct file *file, char __user *userbuf,
 				      dfile->buffer,
 				      dfile->data_len);
 	if (*ppos >= dfile->data_len) {
-		free_pages((unsigned long)dfile->buffer, buforder);
+		kfree((void *)(unsigned long)dfile->buffer);
 		dfile->buffer = NULL;
 		dfile->data_len = 0;
 	}
@@ -577,7 +577,7 @@ static ssize_t b43_debugfs_write(struct file *file,
 		goto out_unlock;
 	}
 
-	buf = (char *)get_zeroed_page(GFP_KERNEL);
+	buf = kzalloc(PAGE_SIZE, GFP_KERNEL);
 	if (!buf) {
 		err = -ENOMEM;
 		goto out_unlock;
@@ -591,7 +591,7 @@ static ssize_t b43_debugfs_write(struct file *file,
 		goto out_freepage;
 
 out_freepage:
-	free_page((unsigned long)buf);
+	kfree((void *)(unsigned long)buf);
 out_unlock:
 	mutex_unlock(&dev->wl->mutex);
 

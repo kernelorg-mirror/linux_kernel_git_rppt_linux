0ad3e115b53412d52efbc7ba106b7505b11ee761
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-02-10 18:30:57 +0200

    drm/i915/gvt: replace __get_free_pages() with kmalloc()
    The setup_spt_oos() allocates memory with __get_free_pages() and uses it as
    a kernel buffer that does not require access to page metadata. Such usage
    can be served by the kmalloc() family.
    
    Replace use of __get_free_pages() with kmalloc().
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- drivers/gpu/drm/i915/gvt/gtt.c
+++ drivers/gpu/drm/i915/gvt/gtt.c
@@ -34,6 +34,7 @@
  */
 
 #include <linux/vmalloc.h>
+#include <linux/slab.h>
 
 #include <drm/drm_print.h>
 
@@ -2486,7 +2487,7 @@ static void clean_spt_oos(struct intel_gvt *gvt)
 	list_for_each_safe(pos, n, &gtt->oos_page_free_list_head) {
 		oos_page = container_of(pos, struct intel_vgpu_oos_page, list);
 		list_del(&oos_page->list);
-		free_page((unsigned long)oos_page->mem);
+		kfree((void *)(unsigned long)oos_page->mem);
 		kfree(oos_page);
 	}
 }
@@ -2507,7 +2508,7 @@ static int setup_spt_oos(struct intel_gvt *gvt)
 			ret = -ENOMEM;
 			goto fail;
 		}
-		oos_page->mem = (void *)__get_free_pages(GFP_KERNEL, 0);
+		oos_page->mem = kmalloc(PAGE_SIZE, GFP_KERNEL);
 		if (!oos_page->mem) {
 			ret = -ENOMEM;
 			kfree(oos_page);

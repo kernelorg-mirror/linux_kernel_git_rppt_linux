91a23d978bbb88e5b7460e34e464a5923d29911f
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-02-10 18:31:52 +0200

    s390/qeth: replace get_zeroed_page() with kzalloc()
    The qeth_get_trap_id() allocates memory with get_zeroed_page() and uses it
    as a kernel buffer that does not require access to page metadata. Such
    usage can be served by the kzalloc() family.
    
    Replace use of get_zeroed_page() with kzalloc().
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- drivers/s390/net/qeth_core_main.c
+++ drivers/s390/net/qeth_core_main.c
@@ -3362,7 +3362,7 @@ static int qeth_query_setdiagass(struct qeth_card *card)
 
 static void qeth_get_trap_id(struct qeth_card *card, struct qeth_trap_id *tid)
 {
-	unsigned long info = get_zeroed_page(GFP_KERNEL);
+	unsigned long info = (unsigned long)kzalloc(PAGE_SIZE, GFP_KERNEL);
 	struct sysinfo_2_2_2 *info222 = (struct sysinfo_2_2_2 *)info;
 	struct sysinfo_3_2_2 *info322 = (struct sysinfo_3_2_2 *)info;
 	struct ccw_dev_id ccwid;
@@ -3381,7 +3381,7 @@ static void qeth_get_trap_id(struct qeth_card *card, struct qeth_trap_id *tid)
 		EBCASC(info322->vm[0].name, sizeof(info322->vm[0].name));
 		memcpy(tid->vmname, info322->vm[0].name, sizeof(tid->vmname));
 	}
-	free_page(info);
+	kfree((void *)info);
 }
 
 static int qeth_hw_trap_cb(struct qeth_card *card,

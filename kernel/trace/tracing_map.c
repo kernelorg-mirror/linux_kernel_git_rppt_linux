0b8a53a737e2b502954e96a0c988c1611a79d33d
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-02-10 18:32:12 +0200

    tracing: replace get_zeroed_page() with kzalloc()
    The tracing_map_array_alloc() allocates memory with get_zeroed_page() and
    uses it as a kernel buffer that does not require access to page metadata.
    Such usage can be served by the kzalloc() family.
    
    Replace use of get_zeroed_page() with kzalloc().
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- kernel/trace/tracing_map.c
+++ kernel/trace/tracing_map.c
@@ -309,7 +309,7 @@ static void tracing_map_array_free(struct tracing_map_array *a)
 		if (!a->pages[i])
 			break;
 		kmemleak_free(a->pages[i]);
-		free_page((unsigned long)a->pages[i]);
+		kfree((void *)(unsigned long)a->pages[i]);
 	}
 
 	kfree(a->pages);
@@ -341,7 +341,7 @@ static struct tracing_map_array *tracing_map_array_alloc(unsigned int n_elts,
 		goto free;
 
 	for (i = 0; i < a->n_pages; i++) {
-		a->pages[i] = (void *)get_zeroed_page(GFP_KERNEL);
+		a->pages[i] = kzalloc(PAGE_SIZE, GFP_KERNEL);
 		if (!a->pages[i])
 			goto free;
 		kmemleak_alloc(a->pages[i], PAGE_SIZE, 1, GFP_KERNEL);

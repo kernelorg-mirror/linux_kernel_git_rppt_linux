370b9d7a0a4e9d3fc63edb511899bf9bd7468782
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-02-10 18:30:25 +0200

    IB/mlx4: replace get_zeroed_page() with kzalloc()
    The mlx4_ib_rereg_user_mr() allocates memory with get_zeroed_page() and
    uses it as a kernel buffer that does not require access to page metadata.
    Such usage can be served by the kzalloc() family.
    
    Replace use of get_zeroed_page() with kzalloc().
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- drivers/infiniband/hw/mlx4/mr.c
+++ drivers/infiniband/hw/mlx4/mr.c
@@ -304,7 +304,7 @@ mlx4_alloc_priv_pages(struct ib_device *device,
 				    MLX4_MR_PAGES_ALIGN);
 
 	/* Prevent cross page boundary allocation. */
-	mr->pages = (__be64 *)get_zeroed_page(GFP_KERNEL);
+	mr->pages = kzalloc(PAGE_SIZE, GFP_KERNEL);
 	if (!mr->pages)
 		return -ENOMEM;
 
@@ -319,7 +319,7 @@ mlx4_alloc_priv_pages(struct ib_device *device,
 	return 0;
 
 err:
-	free_page((unsigned long)mr->pages);
+	kfree((void *)(unsigned long)mr->pages);
 	return ret;
 }
 
@@ -331,7 +331,7 @@ mlx4_free_priv_pages(struct mlx4_ib_mr *mr)
 
 		dma_unmap_single(device->dev.parent, mr->page_map,
 				 mr->page_map_size, DMA_TO_DEVICE);
-		free_page((unsigned long)mr->pages);
+		kfree((void *)(unsigned long)mr->pages);
 		mr->pages = NULL;
 	}
 }

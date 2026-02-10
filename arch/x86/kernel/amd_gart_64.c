ce1620574b9734d6680e74b8ad77e22d6e2f01f3
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-02-10 18:32:09 +0200

    x86/dma/amd_gart: replace get_zeroed_page() with kzalloc()
    The init_amd_gatt() allocates memory with get_zeroed_page() and uses it as
    a kernel buffer that does not require access to page metadata. Such usage
    can be served by the kzalloc() family.
    
    Replace use of get_zeroed_page() with kzalloc().
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- arch/x86/kernel/amd_gart_64.c
+++ arch/x86/kernel/amd_gart_64.c
@@ -12,6 +12,7 @@
  */
 
 #include <linux/types.h>
+#include <linux/slab.h>
 #include <linux/ctype.h>
 #include <linux/agp_backend.h>
 #include <linux/init.h>
@@ -803,7 +804,7 @@ int __init gart_iommu_init(void)
 	 * Any prefetches that hit unmapped entries won't get an bus abort
 	 * then. (P2P bridge may be prefetching on DMA reads).
 	 */
-	scratch = get_zeroed_page(GFP_KERNEL);
+	scratch = (unsigned long)kzalloc(PAGE_SIZE, GFP_KERNEL);
 	if (!scratch)
 		panic("Cannot allocate iommu scratch page");
 	gart_unmapped_entry = GPTE_ENCODE(__pa(scratch));

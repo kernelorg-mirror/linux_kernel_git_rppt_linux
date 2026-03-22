0fe7a3c8d2ab52793584d62db524d31d53beb71b
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-03-22 21:37:27 +0200

    dev: move read_file, read_num and write_num to vm_util
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- tools/testing/selftests/mm/vm_util.c
+++ tools/testing/selftests/mm/vm_util.c
@@ -699,6 +699,27 @@ int unpoison_memory(unsigned long pfn)
 	return ret > 0 ? 0 : -errno;
 }
 
+int read_file(const char *path, char *buf, size_t buflen)
+{
+	int fd;
+	ssize_t numread;
+
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return 0;
+
+	numread = read(fd, buf, buflen - 1);
+	if (numread < 1) {
+		close(fd);
+		return 0;
+	}
+
+	buf[numread] = '\0';
+	close(fd);
+
+	return (unsigned int) numread;
+}
+
 void write_file(const char *path, const char *buf, size_t buflen)
 {
 	int fd;
@@ -712,4 +733,22 @@ void write_file(const char *path, const char *buf, size_t buflen)
 	close(fd);
 	if (numwritten < 1)
 		ksft_exit_fail_msg("Write failed\n");
+}
+
+unsigned long read_num(const char *path)
+{
+	char buf[21];
+
+	if (read_file(path, buf, sizeof(buf)) < 0)
+		ksft_exit_fail_perror("read_file()");
+
+	return strtoul(buf, NULL, 10);
+}
+
+void write_num(const char *path, unsigned long num)
+{
+	char buf[21];
+
+	sprintf(buf, "%ld", num);
+	write_file(path, buf, strlen(buf) + 1);
 }

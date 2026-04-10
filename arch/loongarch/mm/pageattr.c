4a9421749ab5f285e261780b37f6d78b1d3d4169
Nikita Kalyazin <nikita.kalyazin@linux.dev>
2026-04-10 15:17:58 +0000

    set_memory: set_direct_map_* to take address
    Let's convert set_direct_map_*() to take an address instead of a page to
    prepare for adding helpers that operate on folios; it will be more
    efficient to convert from a folio directly to an address without going
    through a page first.
    
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>
    Signed-off-by: Nikita Kalyazin <nikita.kalyazin@linux.dev>
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- arch/loongarch/mm/pageattr.c
+++ arch/loongarch/mm/pageattr.c
@@ -198,9 +198,9 @@ bool kernel_page_present(struct page *page)
 	return pte_present(ptep_get(pte));
 }
 
-int set_direct_map_default_noflush(struct page *page)
+int set_direct_map_default_noflush(const void *_addr)
 {
-	unsigned long addr = (unsigned long)page_address(page);
+	unsigned long addr = (unsigned long)_addr;
 
 	if (addr < vm_map_base)
 		return 0;
@@ -208,19 +208,21 @@ int set_direct_map_default_noflush(struct page *page)
 	return __set_memory(addr, 1, PAGE_KERNEL, __pgprot(0));
 }
 
-int set_direct_map_invalid_noflush(struct page *page)
+int set_direct_map_invalid_noflush(const void *_addr)
 {
-	unsigned long addr = (unsigned long)page_address(page);
+	unsigned long addr = (unsigned long)_addr;
 
 	if (addr < vm_map_base)
 		return 0;
 
-	return __set_memory(addr, 1, __pgprot(0), __pgprot(_PAGE_PRESENT | _PAGE_VALID));
+	return __set_memory(addr, 1, __pgprot(0),
+			    __pgprot(_PAGE_PRESENT | _PAGE_VALID));
 }
 
-int set_direct_map_valid_noflush(struct page *page, unsigned nr, bool valid)
+int set_direct_map_valid_noflush(const void *_addr, unsigned long numpages,
+				 bool valid)
 {
-	unsigned long addr = (unsigned long)page_address(page);
+	unsigned long addr = (unsigned long)_addr;
 	pgprot_t set, clear;
 
 	if (addr < vm_map_base)
@@ -234,5 +236,5 @@ int set_direct_map_valid_noflush(struct page *page, unsigned nr, bool valid)
 		clear = __pgprot(_PAGE_PRESENT | _PAGE_VALID);
 	}
 
-	return __set_memory(addr, nr, set, clear);
+	return __set_memory(addr, numpages, set, clear);
 }

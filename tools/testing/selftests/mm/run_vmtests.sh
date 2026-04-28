ef67b7d94f0efca4cdd4525ff950fd269bd0f638
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-04-28 23:42:39 +0300

    selftests/mm: run_vmtests.sh: free memory if available memory is low
    Currently when running THP and HugeTLB tests, if HAVE_HUGEPAGES is set
    run_test() drops caches, compacts memory and runs the test. But if
    HAVE_HUGEPAGES is not set it skips the tests entirely, even if THP tests
    have nothing to do with HAVE_HUGEPAGES.
    
    Replace the check if HAVE_HUGEPAGES is set with a check of how much memory
    is available. If there is less than 256 MB of available memory, drop caches
    and run compaction and then continue to run a test regardless of
    HAVE_HUGEPAGES value.
    
    Tested-by: Luiz Capitulino <luizcap@redhat.com>
    Tested-by: Sarthak Sharma <sarthak.sharma@arm.com>
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- tools/testing/selftests/mm/run_vmtests.sh
+++ tools/testing/selftests/mm/run_vmtests.sh
@@ -239,14 +239,14 @@ run_test() {
 		# On memory constrainted systems some tests can fail to allocate hugepages.
 		# perform some cleanup before the test for a higher success rate.
 		if [ ${CATEGORY} == "thp" -o ${CATEGORY} == "hugetlb" ]; then
-			if [ "${HAVE_HUGEPAGES}" = "1" ]; then
+			mem_kb=$(awk '/MemAvailable/ {print $2}' /proc/meminfo)
+			mem_Mb=$((mem_available_kb / 1024))
+
+			if (( $mem_Mb < 256 )); then
 				echo 3 > /proc/sys/vm/drop_caches
 				sleep 2
 				echo 1 > /proc/sys/vm/compact_memory
 				sleep 2
-			else
-				echo "hugepages not supported" | tap_prefix
-				skip=1
 			fi
 		fi
 

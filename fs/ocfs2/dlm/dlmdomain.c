5c3135c4f592edd819a680bd6c29397ef059a751
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-05-17 19:38:17 +0300

    ocfs2/dlm: replace __get_free_page() with kmalloc()
    The dlm_alloc_pagevec() allocates memory with __get_free_page() and uses it
    as a kernel buffer that does not require access to page metadata. Such
    usage can be served by the kmalloc() family.
    
    Replace use of __get_free_page() with kmalloc().
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- fs/ocfs2/dlm/dlmdomain.c
+++ fs/ocfs2/dlm/dlmdomain.c
@@ -63,7 +63,7 @@ static inline void byte_copymap(u8 dmap[], unsigned long smap[],
 static void dlm_free_pagevec(void **vec, int pages)
 {
 	while (pages--)
-		free_page((unsigned long)vec[pages]);
+		kfree((void *)(unsigned long)vec[pages]);
 	kfree(vec);
 }
 
@@ -76,7 +76,7 @@ static void **dlm_alloc_pagevec(int pages)
 		return NULL;
 
 	for (i = 0; i < pages; i++)
-		if (!(vec[i] = (void *)__get_free_page(GFP_KERNEL)))
+		if (!(vec[i] = kmalloc(PAGE_SIZE, GFP_KERNEL)))
 			goto out_free;
 
 	mlog(0, "Allocated DLM hash pagevec; %d pages (%lu expected), %lu buckets per page\n",

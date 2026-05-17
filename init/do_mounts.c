00c35b4f10a1f51f079d87fe2be314418a189c86
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-05-17 19:44:42 +0300

    init: replace __get_free_page() with kmalloc()
    The kernel buffer allocated with __get_free_page() in this site is used as
    a kernel-only buffer that does not require access to page metadata. Such
    usage can be served by the kmalloc() family.
    
    Replace use of __get_free_page() with kmalloc() for this allocation and its
    paired free. Other allocators in the same file are left untouched.
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -343,7 +343,7 @@ static int __init mount_nodev_root(char *root_device_name)
 	int err = -EINVAL;
 	int num_fs, i;
 
-	fs_names = (void *)__get_free_page(GFP_KERNEL);
+	fs_names = kmalloc(PAGE_SIZE, GFP_KERNEL);
 	if (!fs_names)
 		return -EINVAL;
 	num_fs = split_fs_names(fs_names, PAGE_SIZE);
@@ -360,7 +360,7 @@ static int __init mount_nodev_root(char *root_device_name)
 			break;
 	}
 
-	free_page((unsigned long)fs_names);
+	kfree((void *)(unsigned long)fs_names);
 	return err;
 }
 

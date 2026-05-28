4cfa32fe92b38d69c306ae79e6f464bb2a57b6c1
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-05-28 08:39:41 +0300

    wifi: wlcore: replace __get_free_pages() with kzalloc()
    wlcore_alloc_hw() uses __get_free_pages() to  allocate TX aggregation
    and firmware log buffers used for software data staging.
    
    These buffer can be allocated with kmalloc() as there's nothing special
    about them to go directly to the page allocator.
    
    kmalloc() provides a better API that does not require ugly casts and
    kfree() does not need to know the size of the freed object.
    
    Performance difference between kmalloc() and __get_free_pages() is not
    measurable as both allocators take an object/page from a per-CPU list for
    fast path allocations.
    
    For the slow path the performance is anyway determined by the amount of
    reclaim involved rather than by what allocator is used.
    
    Replace use of __get_free_pages() with kzalloc() and free_pages() with
    kfree().
    
    Link: https://lore.kernel.org/all/635405e4-9423-4a25-a6e7-e03c8ea0bcbe@redhat.com
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- drivers/net/wireless/ti/wlcore/main.c
+++ drivers/net/wireless/ti/wlcore/main.c
@@ -6351,7 +6351,6 @@ struct ieee80211_hw *wlcore_alloc_hw(size_t priv_size, u32 aggr_buf_size,
 	struct ieee80211_hw *hw;
 	struct wl1271 *wl;
 	int i, j, ret;
-	unsigned int order;
 
 	hw = ieee80211_alloc_hw(sizeof(*wl), &wl1271_ops);
 	if (!hw) {
@@ -6431,8 +6430,7 @@ struct ieee80211_hw *wlcore_alloc_hw(size_t priv_size, u32 aggr_buf_size,
 	mutex_init(&wl->flush_mutex);
 	init_completion(&wl->nvs_loading_complete);
 
-	order = get_order(aggr_buf_size);
-	wl->aggr_buf = (u8 *)__get_free_pages(GFP_KERNEL, order);
+	wl->aggr_buf = kmalloc(round_up(aggr_buf_size, PAGE_SIZE), GFP_KERNEL);
 	if (!wl->aggr_buf) {
 		ret = -ENOMEM;
 		goto err_wq;
@@ -6446,7 +6444,7 @@ struct ieee80211_hw *wlcore_alloc_hw(size_t priv_size, u32 aggr_buf_size,
 	}
 
 	/* Allocate one page for the FW log */
-	wl->fwlog = (u8 *)get_zeroed_page(GFP_KERNEL);
+	wl->fwlog = kzalloc(PAGE_SIZE, GFP_KERNEL);
 	if (!wl->fwlog) {
 		ret = -ENOMEM;
 		goto err_dummy_packet;
@@ -6471,13 +6469,13 @@ err_mbox:
 	kfree(wl->mbox);
 
 err_fwlog:
-	free_page((unsigned long)wl->fwlog);
+	kfree(wl->fwlog);
 
 err_dummy_packet:
 	dev_kfree_skb(wl->dummy_packet);
 
 err_aggr:
-	free_pages((unsigned long)wl->aggr_buf, order);
+	kfree(wl->aggr_buf);
 
 err_wq:
 	destroy_workqueue(wl->freezable_wq);
@@ -6506,9 +6504,9 @@ int wlcore_free_hw(struct wl1271 *wl)
 
 	kfree(wl->buffer_32);
 	kfree(wl->mbox);
-	free_page((unsigned long)wl->fwlog);
+	kfree(wl->fwlog);
 	dev_kfree_skb(wl->dummy_packet);
-	free_pages((unsigned long)wl->aggr_buf, get_order(wl->aggr_buf_size));
+	kfree(wl->aggr_buf);
 
 	wl1271_debugfs_exit(wl);
 
